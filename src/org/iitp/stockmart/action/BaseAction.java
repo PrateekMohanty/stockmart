034fa98aa2a92aca45db321304d59c42f722efee
ayushmaanbhav <ayush.cs11@iitp.ac.in>
2014-08-31 02:58:11 +0530

    setting up a template so that it is easy to work

---
+++ src/org/iitp/stockmart/action/BaseAction.java
@@ -0,0 +1,11 @@
+package org.iitp.stockmart.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+	public String execute() {
+		return "success";
+	}
+	
+}
